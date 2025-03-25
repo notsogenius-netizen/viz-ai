7609fa6e39810f8b8b685ddfc92d2d23a180f3bd
notsogenius-netizen <sourabhchangmai11@gmail.com>
2025-03-25 11:00:40 +0530

    Refactor JWT token handling to improve error management and set default expiration time

--- app/utils/jwt.py
+++ app/utils/jwt.py
@@ -1,24 +1,27 @@
 from fastapi import HTTPException, status
 from datetime import datetime, timedelta, timezone
-from jose import jwt, JWTError
+from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
 from app.core.settings import settings
+import jwt
 
 def create_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + expires_delta
+    
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=120))
+    
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode,key= settings.SECRET_KEY, algorithm= settings.ALGORITHM)
+    
+    return jwt.encode(to_encode, key=settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
 def decode_token(token: str):
     try:
-        return jwt.decode(token,key= settings.SECRET_KEY, algorithms= settings.ALGORITHM)
-    except jwt.ExpiredSignatureError:
+        return jwt.decode(token, key=settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    except ExpiredSignatureError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, 
             detail="Token has expired"
         )
-    
-    except JWTError:
+    except InvalidTokenError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, 
             detail="Invalid token"
